54788b41535c0866890bc8e6390ed46baa68f7a4
Davis0uza <58854976+Davis0uza@users.noreply.github.com>
2025-10-24 01:13:22 +0100

    cleanup

--- choosewine-backend/src/models/Favorite.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const FavoriteSchema = new mongoose.Schema({
-  user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-  wine: { type: mongoose.Schema.Types.ObjectId, ref: 'Wine', required: true }
-});
-
-module.exports = mongoose.model('Favorite', FavoriteSchema);
--- choosewine-backend/src/models/History.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const HistorySchema = new mongoose.Schema({
-  user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-  wine: { type: mongoose.Schema.Types.ObjectId, ref: 'Wine', required: true },
-  accessed_at: { type: Date, default: Date.now }
-});
-
-module.exports = mongoose.model('History', HistorySchema);
